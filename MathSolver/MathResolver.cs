8b631b77a0cc7206d2625115e7834c1ef46a5f4a
Richard <manhkhang@gmx.de>
2019-05-27 01:08:42 +0200

    Projektdateien hinzufügen.

---
+++ MathSolver/MathResolver.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathSolver
+{
+    class MathResolver
+    {
+
+
+
+
+        public static string resolve(string problem)
+        {
+            int solution;
+            List<Operators> operators = new List<Operators>();
+
+            char value = problem.First();
+            problem = problem.Remove(0,1);
+
+            foreach(char symbol in problem)
+            {
+                operators.Add(new Operators(symbol));
+            }
+
+
+
+
+
+
+            return problem.ToString();
+            //return solution;
+        }
+
+
+
+
+
+
+    }
+}
---
+++ MathSolver/Operators.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathSolver
+{
+    class Operators
+    {
+        int value;
+        char sign;
+
+        public Operators(char symbol)
+        {
+            try
+            {
+                value = Convert.ToInt32(symbol);
+            }
+            catch (Exception ex)
+            {
+                value = 0;
+            }
+        }
+    }
+}
